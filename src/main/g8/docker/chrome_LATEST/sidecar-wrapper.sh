5ea4318a4815a21865d416f31213b0251f490623
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2020-05-15 17:21:59 +0100

    PLATUI-476: Updated test-ui-test-template script to build and run docker images during the test run updated webdriver-factory version

---
+++ src/main/g8/docker/chrome_LATEST/sidecar-wrapper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+IFS=',' read -r -a PORT_MAPPINGS_ARRAY <<< \$PORT_MAPPINGS
+
+for PORT_MAPPING in "\${PORT_MAPPINGS_ARRAY[@]}"; do
+  IFS='>' read -r -a PORT <<< \$PORT_MAPPING
+  echo "Mapping localhost:\${PORT[0]::-1} to \$TARGET_IP:\${PORT[1]}"
+  echo "localhost \${PORT[0]::-1} \$TARGET_IP \${PORT[1]}" >> ~/rinetd.conf
+done
+
+rinetd --conf-file ~/rinetd.conf --foreground &
+
+/opt/bin/entry_point.sh
