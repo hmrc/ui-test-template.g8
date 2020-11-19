77d50ee53278fe5372fd6f3e937359c01812828c
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2020-11-19 16:06:17 +0000

    PLATUI-809 - Formatting Cucumber project files for test template.

--- src/main/g8/src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
+++ src/main/g8/src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
@@ -6,7 +6,6 @@ import org.scalatest.matchers.should.Matchers
 trait BasePage extends Matchers {
   val url: String
 }
-
 $else$
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.selenium.{Page, WebBrowser}
